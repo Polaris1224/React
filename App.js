bdeb424bad6b4abe9dcfeb0fa1628373dc13e0aa
Polaris <118846149+Polaris1224@users.noreply.github.com>
2025-03-16 22:29:55 +0530

    React Components : Class Based Components

---
+++ App.js
@@ -0,0 +1,14 @@
+import MovieCard from "./MovieCard";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Movie App</h1>
+      <MovieCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
