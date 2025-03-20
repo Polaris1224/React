fd5dc6d331eb97479d42fbaaee72e86f56d34450
Polaris <118846149+Polaris1224@users.noreply.github.com>
2025-03-20 17:10:36 +0530

    Context APIs part 1

---
+++ Context API/Context API 1/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import styles from "../styles/Total.module.css";
+import { itemContext } from "../itemContext";
+import { totalContext } from "../totalContext";
+
+function Navbar() {
+  const value = useContext(itemContext) ;
+  const {total} = useContext(totalContext) ;
+
+  return (
+    <div className={styles.container}>
+      <h1>Total : &#x20B9; { total }</h1>
+      <h1>Items: { value.item }</h1>
+    </div>
+  );
+}
+
+export default Navbar;
